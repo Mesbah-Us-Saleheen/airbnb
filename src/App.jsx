8d4c38d0abb99bb17699138b8be55a8789146ec1
mesbah <mesbahsaleheen@yahoo.com>
2024-10-08 18:42:45 +0600

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from './components/Navbar.jsx'
+// import Hero from './components/Hero.jsx'
+import Card from './components/Card.jsx'
+import data from './data.js'
+import './App_custom.css'
+// import './App.css'
+
+function App() {
+
+  const cards = data.map(item => {
+    return(
+      <Card
+        {...item} //spread syntax in object literals
+      />
+    )
+  })
+  return (
+    <>
+      <Navbar />
+      {/*<Hero />*/}
+      <section className="cards-list">
+        {cards}
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+	return (
+		<>
+			<nav>
+				<img src="img/airbnb.png" alt="logo" />
+			</nav>
+		</>
+	)
+}
