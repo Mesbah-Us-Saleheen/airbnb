f32e64fef5dd11407e6d54245827722d53cb0c5e
Mesbah-Us-Saleheen <122684874+Mesbah-Us-Saleheen@users.noreply.github.com>
2024-10-08 18:58:20 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar.jsx'
-// import Hero from './components/Hero.jsx'
+import Hero from './components/Hero.jsx'
 import Card from './components/Card.jsx'
 import data from './data.js'
 import './App_custom.css'
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <Navbar />
-      {/*<Hero />*/}
+      <Hero />
       <section className="cards-list">
         {cards}
       </section>
